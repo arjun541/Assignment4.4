01ea6b854d14166b661ff1d226ab8e72b8b380fc
arjun541 <bmkr.541@gmail.com>
2017-01-26 09:38:03 +0530

    Create ChildClass.java

---
+++ ChildClass.java
@@ -0,0 +1,14 @@
+
+
+public class ChildClass extends ParentClass{
+
+	public  ChildClass()
+	{      super();
+		System.out.println("Child Class Constructor message");
+	}
+	
+	public static void main(String a[])
+	{
+		ChildClass c=new ChildClass();
+	}
+}
